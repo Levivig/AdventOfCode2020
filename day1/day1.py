6978300be5f1685f44ab723b0a18fa39011ef34b
Levente Vig <levivig@gmail.com>
2020-12-01 06:35:17 +0100

    ✨ Day 1 solutions added
    https://adventofcode.com/2020/day/1

--- day1/day1.py
+++ day1/day1.py
@@ -3,6 +3,7 @@
 
 
 import os
+import time
 
 
 dirname = os.path.dirname(__file__)
@@ -11,14 +12,42 @@ inputfile = os.path.join(dirname, 'input.txt')
 
 def part1(data):
     print('part1:')
-    for line in data:
-        print(line)
+    found = False
+    for line1 in data:
+        n1 = int(line1)
+        if n1 > 2020:
+            continue
+        for idx, line2 in enumerate(data):
+            n2 = int(line2)
+            if idx == 0 or n2 > 2020:
+                continue
+            if n1 + n2 == 2020:
+                solution = n1 * n2
+                print(solution)
+                return solution
 
 
 def part2(data):
     print('part2:')
-    for line in data:
-        print(line)
+    for line1 in data:
+        n1 = int(line1)
+        if n1 > 2020:
+            continue
+        for idx, line2 in enumerate(data):
+            n2 = int(line2)
+            if idx == 0 or n2 > 2020:
+                continue
+            for idx1, line3 in enumerate(data):
+                n3 = int(line3)
+                if idx == 1 or n3 > 2020:
+                    continue
+                currentSum = n1 + n2
+                if currentSum > 2020:
+                    break
+                if currentSum + n3 == 2020:
+                    solution = n1 * n2 * n3
+                    print(solution)
+                    return solution
 
 
 def main():
@@ -26,8 +55,10 @@ def main():
     print(currentDay)
     with open(inputfile) as f:
         data = f.read().splitlines()
-        part1(data)
-        part2(data)
+        start_time = time.time()
+        assert part1(data) == 1020084
+        assert part2(data) == 295086480
+        print("--- %s seconds ---" % (time.time() - start_time))
 
 
 if __name__ == "__main__":
