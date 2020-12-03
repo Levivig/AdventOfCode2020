27d906a499341f3627dc9f56392beab427ab92e6
Levente Vig <levivig@gmail.com>
2020-12-03 06:32:48 +0100

    ✨Day 3 solutions added
    https://adventofcode.com/2020/day/3

--- day3/day3.py
+++ day3/day3.py
@@ -12,14 +12,52 @@ inputfile = os.path.join(dirname, 'input.txt')
 
 def part1(data):
     print('part1:')
+    x = 0
+    y = 0
+
+    trees = 0
     for line in data:
-        print(line)
+        lineLenght = len(line)
+        currentItem = line[x%lineLenght]
+        if currentItem == "#":
+            trees += 1
+        x += 3
+        y += 1
+    print(trees)
+    return trees
 
 
 def part2(data):
     print('part2:')
-    for line in data:
-        print(line)
+    x = [0,0,0,0,0]
+    y = [0,0,0,0,0]
+
+    moves = [(1,1), (3,1), (5,1), (7,1),(1,2)] 
+
+    trees = [0,0,0,0,0]
+    for n, line in enumerate(data):
+        lineLenght = len(line)
+
+        for idx, move in enumerate(moves):
+            if idx == 4 and n % 2 != 0:
+                continue
+            currentX = x[idx]
+            currentY = y[idx]
+
+            currentItem = line[currentX%lineLenght]
+            if currentItem == "#":
+                trees[idx] += 1
+            x[idx] += int(move[0])
+            y[idx] += int(move[1])
+
+    print(trees)
+
+    mult = 1
+    for tree in trees:
+        mult *= tree
+    print(mult)
+
+    return mult
 
 
 def main():
@@ -28,8 +66,8 @@ def main():
     with open(inputfile) as f:
         data = f.read().splitlines()
         start_time = time.time()
-        part1(data)
-        part2(data)
+        assert part1(data) == 209
+        assert part2(data) == 1574890240
         print("--- %s seconds ---" % (time.time() - start_time))
 
 
